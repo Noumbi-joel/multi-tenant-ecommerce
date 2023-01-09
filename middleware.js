b5a130edaae360888e076d1bb09833a80532012d
Little Jacob <joelnoumbi28@gmail.com>
2023-01-09 19:42:26 +0100

    config multi tenant folder structure

--- middleware.js
+++ middleware.js
@@ -2,7 +2,6 @@ import { NextResponse } from "next/server";
 
 export default function middleware(req) {
   let connected = req.cookies.get("user");
-  let noBusiness = req.cookies.get("noBusiness");
   let url = req.url;
 
   if (
